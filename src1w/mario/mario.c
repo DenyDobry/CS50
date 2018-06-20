9f7ca49f451a7f91b13654355bdc206adcb716e8
DenyDobry <deny.bonner@gmail.com>
2018-06-20 20:15:50 +0300

    mario complete

--- src1w/mario/mario.c
+++ src1w/mario/mario.c
@@ -4,17 +4,37 @@
 int GetCorrectInt(void);
 
 int main(void) {	
-	int height;
+	int height; 
+	int countString;
+	int backCountString;
+	int spaces;
+	int sharps;
+
 	height = GetCorrectInt();
+	backCountString = height;
+	for (countString = 1; countString <= height; countString++)
+	{
+		for (spaces = 1; spaces < backCountString; spaces++)
+		{
+			printf(" ");
+		}
+		for (sharps = 0; sharps <= countString; sharps++)
+		{
+			printf("#");
+		}
+		printf("\n");
+		backCountString--;
+	}
 
 }
 
-int GetCorrectInt(void) {
+int GetCorrectInt(void) 
+{
 	int corr;
 	do {
 		printf("Enter height: ");
 		corr = GetInt();
 	}
-	while (corr < 1 || corr > 24);
+	while (corr < 1 || corr > 23);
 	return corr;
 }
